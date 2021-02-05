13e4bd1d7057c807cc706acec23484add130fdb8
ryu <onkeyi@gmail.com>
2021-02-05 16:26:01 +0900

    :seedling: Api key middleware

---
+++ app/Http/Middleware/AuthenticateApiKey.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\ApiKeyNotfoundException;
+use Closure;
+use Illuminate\Http\Request;
+
+class AuthenticateApiKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $apiKeyValue = $request->header('API-KEY');
+
+        if (!isset($apiKeyValue) || env('API_KEY') !== $apiKeyValue) {
+            throw new ApiKeyNotfoundException();
+        }
+        return $next($request);
+    }
+}
