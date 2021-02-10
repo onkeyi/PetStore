662d4af42e1dd8c3324fc08892d139ee46853c98
ryu <onkeyi@gmail.com>
2021-02-10 14:41:28 +0900

    email validation

--- app/Http/Requests/AuthRegisterRequest.php
+++ app/Http/Requests/AuthRegisterRequest.php
@@ -25,7 +25,7 @@ class AuthRegisterRequest extends ApiRequest
     {
         return [
             'name' => 'required',
-            'email' => ['required', 'unique:users'],
+            'email' => ['required', 'email', 'unique:users'],
             'password' => ['required', 'min:6']
         ];
     }
